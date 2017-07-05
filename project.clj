95bb6420be9a69d00c5f646d59071d7f4f6732a8
Kyle Harrington <kharrington@uidaho.edu>
2017-07-05 20:12:17 +0200

    Version bump

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject brevis.us/brevis-utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject brevis.us/brevis-utils "0.1.0"
+  :description "Utilities from the Brevis suite"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
