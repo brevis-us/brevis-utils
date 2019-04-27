eb70f90d49854a780fb95ec91f4c2013b80d2432
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-04-27 10:56:01 -0400

    Point to clj-random with groupId

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-
                  [me.raynes/conch "0.8.0"]
                  
                  ;; Math
                  [net.mikera/core.matrix "0.48.0"]
                  [net.mikera/vectorz-clj "0.39.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [clj-random "0.1.7"]]
-  :repositories [["snapshots" {:url "https://clojars.org/repo"
+                 [com.kephale/clj-random "21e3dd4168"]]
+  :repositories [["jitpack" "https://jitpack.io"]
+		 ["snapshots" {:url "https://clojars.org/repo"
                                :username :env/CI_DEPLOY_USERNAME
                                :password :env/CI_DEPLOY_PASSWORD
                                :sign-releases false}]
